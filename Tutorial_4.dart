3e724227adaff34b9591015b80873ff2a24cf261
Limtotage <abdullahltgyalim@gmail.com>
2025-03-28 22:45:28 +0300

    Create Tutorial_4.dart

---
+++ Tutorial_4.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WidgetDetail());
+}
+
+class WidgetDetail extends StatelessWidget {
+  const WidgetDetail({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Widget Detail"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          leading :  Text("Menu"),
+        ),
+        body : const Center(
+          child : Text("Hello Flutter"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){},
+          child: const Text("+"), 
+        ),
+      ),
+    );
+  }
+}
