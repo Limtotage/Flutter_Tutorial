78b777932c58a584671241e201d15dc1968da0ab
Limtotage <abdullahltgyalim@gmail.com>
2025-03-28 23:14:40 +0300

    Convert Stateless Widget to Statefull Widget

--- Tutorial_4.dart
+++ Tutorial_4.dart
@@ -3,7 +3,39 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const WidgetDetail());
 }
-
+class WidgetDetail extends StatefulWidget {
+  const WidgetDetail({super.key});
+  @override
+  State<WidgetDetail> createState() => _WidgetDetailState();
+}
+class _WidgetDetailState extends State<WidgetDetail> {
+  int value =0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Widget Detail"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          leading :  Text("Menu"),
+        ),
+        body : Center(
+          child : Text(value.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){
+            setState((){
+              value+=1;
+            });
+          },
+          child: const Text("+"), 
+        ),
+      ),
+    );
+  }
+}
+/*
 class WidgetDetail extends StatelessWidget {
   const WidgetDetail({super.key});
   @override
@@ -27,3 +59,35 @@ class WidgetDetail extends StatelessWidget {
     );
   }
 }
+class Test extends StatefulWidget {
+  const Test({super.key});
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test>{
+  int value =0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body : Center(child: Column(
+            mainAxisAlignment : MainAxisAlignment.center,
+            children:[
+              Text(value.toString()),
+              ElevatedButton(
+                onPressed:(){
+                  setState( () {
+                    value+=1;
+                  });
+                },
+                child:const Text("Tıkla"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}*/
+
